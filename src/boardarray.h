de9eab61bb2ee0d5fbe920a41f37d125b4925ac9
johnfnkgo <gojohnfrank@gmail.com>
2025-09-22 16:12:31 +0800

    implementing on void add

--- src/boardarray.h
+++ src/boardarray.h
@@ -14,9 +14,30 @@ class BoardArray : public Board {
         }
 
         void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-            return;
+            if (index == 0){
+                array [0] = *entry;
+                index++;
+                return;
+            }
+            
+            int pos = index;
+            for (int i = 0; i < index; i++){
+                if (entry->compare(array[i])){
+                    pos = i;
+                    break;
+                }
+            }
+            if (index == SIZE && pos == index){
+                cout << entry->getName() <<"'s score is too low to be added!" << endl;
+                return;
+            }
+            if (index < SIZE){
+                index++;
+            }
+            for (int j = index -1; j > pos; j--){
+                array[j] = array[j - 1];
+            }
+            array [pos] = *entry;
 
         }
 
